8bcaf478e8b7fcee5ca9e50c38cf6c043e98f3f2
jungho <kiik52@naver.com>
2025-06-20 17:25:21 +0900

    add : 텀블벅 파비콘 추가 및 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,25 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h2>asdljkanmsdlkamsdlkmalskdmalksmdlkm</h2>;
+  return (
+    <div className="w-full min-h-screen overflow-hidden text-base justify-center items-center flex">
+      <ul>
+        <li>
+          <Link href="">임나연</Link>
+        </li>
+        <li>
+          <Link href="">조석근</Link>
+        </li>
+        <li>
+          <Link href="">박준범</Link>
+        </li>
+        <li>
+          <Link href="">이석진</Link>
+        </li>
+        <li>
+          <Link href="">지정호</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
