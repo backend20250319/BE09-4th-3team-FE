ee8a9e5a67b1d790cb3fd6c4f7737948a9c7f989
Ji_Jung_Ho <110534668+hoya-q@users.noreply.github.com>
2025-06-21 12:30:58 +0900

    style: page.jsx 반응형 높이 수정

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Main from "./(main)/Main";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen overflow-hidden text-base justify-center items-center flex">
+    <div className="w-full h-screen overflow-hidden text-base justify-center items-center flex">
       <Main />
     </div>
   );
