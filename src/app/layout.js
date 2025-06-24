256cd63201d9aed01e1155b4fe97e529f421be30
jungho <kiik52@naver.com>
2025-06-24 18:32:20 +0900

    - 공통 헤더 페이지 진행 중

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from "@/components/header/Header";
 import "./globals.css";
 
 export const metadata = {
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
