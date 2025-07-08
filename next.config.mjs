022666641d2cb7f028a6187b84f155133eec5f12
jungho <kiik52@naver.com>
2025-07-08 15:24:43 +0900

    add : 외부 FTP 이미지 서버 next.js의 <Images> 태그에서 사용할 수 있도록 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,25 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
+      // ✅ localhost 이미지 허용
       {
         protocol: "http",
         hostname: "localhost",
         port: "8888",
-        pathname: "/images/**", // 실제 이미지 경로에 따라 조정
+        pathname: "/images/**",
+      },
+      // ✅ 외부 FTP 이미지 서버 허용
+      {
+        protocol: "http",
+        hostname: "dev.macacolabs.site",
+        port: "8008",
+        pathname: "/images/**",
       },
     ],
   },
 };
 
-// 프록시 설정
+// ✅ API 프록시 설정
 export async function rewrites() {
   return [
     {
