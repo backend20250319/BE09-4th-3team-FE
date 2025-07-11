fd18de74b0ad8dd6d7214f92cba0c360e7ad9a63
Ji-Jung-Ho <kiik52@naver.com>
2025-07-11 21:50:29 +0900

    feat : 공통 페이지네이션 추가

--- src/app/(main)/project/list/page.jsx
+++ src/app/(main)/project/list/page.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import Link from "next/link";
 import Image from "next/image";
 import { getDday } from "@/components/utils/dday";
+import Pagination from "@/components/pagination/pagination";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Page() {
   const [projects, setProjects] = useState([]);
@@ -16,6 +18,12 @@ export default function Page() {
   });
   const [approvedCount, setApprovedCount] = useState(0);
 
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  // 페이지 쿼리로부터 초기 값 설정
+  const currentPage = parseInt(searchParams.get("page") || "0", 10);
+
   const fetchProjects = async (page = 0, size = 12) => {
     try {
       const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/project/list`, {
@@ -25,26 +33,26 @@ export default function Page() {
         setProjects(response.data.data);
         setPagination(response.data.pagination);
         setApprovedCount(response.data.approvedCount);
-
-        console.log(response);
       }
     } catch (error) {
       console.error("프로젝트 목록 조회 실패:", error);
     }
   };
 
+  // 쿼리 변화에 따라 fetch
   useEffect(() => {
-    fetchProjects();
-  }, []);
+    fetchProjects(currentPage);
+  }, [currentPage]);
 
+  // 페이지 변경 시 URL 이동 + 스크롤 이동
   const handlePageChange = (newPage) => {
     if (newPage >= 0 && newPage < pagination.totalPages) {
-      fetchProjects(newPage, pagination.size);
+      router.push(`?page=${newPage}`);
     }
   };
 
   return (
-    <div className="p-6 w-[1160px] mx-auto">
+    <div className="w-[1160px] mx-auto">
       <div className="text-base leading-[27px] mx-auto pt-4 pr-5 pb-3">
         <span className="text-[#ff5757]">{approvedCount}</span>개의 프로젝트가 있습니다.
       </div>
@@ -53,7 +61,7 @@ export default function Page() {
       <div className="grid grid-cols-4 gap-4">
         {projects.map((project) => (
           <Link key={project.projectNo} href={`/project/detail/${project.projectNo}`}>
-            <div className="border rounded-[8px] shadow">
+            <div className="rounded-t-[8px] border-b-3 border-[#eb4b38]">
               {project.thumbnailUrl ? (
                 <div className="overflow-hidden rounded-t-[8px]">
                   <Image
@@ -61,61 +69,47 @@ export default function Page() {
                     alt={project.title}
                     width={300}
                     height={300}
-                    className="object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+                    className="object-cover transition-transform rounded-b-[8px] duration-300 ease-in-out hover:scale-110"
                   />
                 </div>
               ) : (
-                <div className="h-[264px] bg-gray-200 flex mb-2 items-center justify-center text-sm text-gray-500 rounded-t-[8px]">
+                <div className="h-[264px] bg-gray-200 flex mb-[14px] items-center justify-center text-sm text-gray-500 rounded-[8px]">
                   이미지 없음
                 </div>
               )}
               {/* 프로젝트 정보 */}
 
-              <div className="px-4 pt-4 pb-2">
+              <div className="pt-4 flex flex-col gap-1">
                 <p className="text-xs leading-[120%] text-[#545454]">{project.creatorName}</p>
-                <h2 className="text-base pb-4 text-[#1c1c1c] mb-[6px] border-b border-[#6d6d6d]">{project.title}</h2>
+                <h2 className="text-base pb-1 text-[#1c1c1c] mb-[6px] border-b ">{project.title}</h2>
               </div>
-              <div className="pb-4 px-4">
-                <p className="text-base text-[#1c1c1c] mb-[6px] flex gap-2 items-center">
-                  목표 금액:
-                  <span className="text-[#545454] text-[15px]">{project.goalAmount.toLocaleString()}원</span>
+              <div className="pb-1 text-sm">
+                <p className="text-[#1c1c1c] mb-[6px] flex gap-2 items-center">
+                  목표금액:
+                  <span className="text-[#545454]">{project.goalAmount.toLocaleString()}원</span>
                 </p>
-                <p className="text-base text-[#1c1c1c] mb-[6px] flex gap-2 items-center">
-                  모인 금액:
-                  <span className="text-[#545454] text-[15px]">{project.currentAmount.toLocaleString()}원</span>
-                </p>
-                <p className="text-base text-[#1c1c1c] mb-[6px] flex gap-2 items-center">
+                <p className="text-[#1c1c1c] mb-[6px] flex gap-2 items-center">
                   마감일:
-                  <span className="text-[#545454] text-[15px]">{project.deadLine}</span>
+                  <span className="text-[#545454]">{project.deadLine}</span>
                 </p>
-                <p className="text-base text-[#1c1c1c] mb-[6px] flex gap-2 items-center">
-                  {getDday(project.startLine, project.deadLine)}일 남음
-                </p>
+                <div className="flex justify-between">
+                  <div className="flex gap-1">
+                    <p className="text-sm text-[#eb4b38] font-bold">{project.percent}%</p>
+                    <span className="text-[#545454] text-sm font-normal">
+                      {project.currentAmount.toLocaleString()}원
+                    </span>
+                  </div>
+                  <p className="text-[13px] text-[#545454] mb-[6px] font-bold">
+                    {getDday(project.startLine, project.deadLine)}일 남음
+                  </p>
+                </div>
               </div>
             </div>
           </Link>
         ))}
       </div>
 
-      <div className="mt-6 flex justify-center items-center space-x-4">
-        <button
-          onClick={() => handlePageChange(pagination.page - 1)}
-          disabled={pagination.page === 0}
-          className="px-3 py-1 border rounded disabled:opacity-50"
-        >
-          이전
-        </button>
-        <span>
-          {pagination.page + 1} / {pagination.totalPages}
-        </span>
-        <button
-          onClick={() => handlePageChange(pagination.page + 1)}
-          disabled={pagination.page + 1 >= pagination.totalPages}
-          className="px-3 py-1 border rounded disabled:opacity-50"
-        >
-          다음
-        </button>
-      </div>
+      <Pagination currentPage={pagination.page} totalPages={pagination.totalPages} onPageChange={handlePageChange} />
     </div>
   );
 }
