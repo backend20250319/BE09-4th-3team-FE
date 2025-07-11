e4c44fc8021d4bfb64e80c74991f539b186cc463
jungho <kiik52@naver.com>
2025-07-11 09:24:02 +0900

    feat : 스크롤Y값에 따라 선물하기 div 고정 및 로그인하지 않은 유저는 결제페이지로 넘어가지 못하게 구현

--- src/app/(main)/project/detail/components/projectInfo.jsx
+++ src/app/(main)/project/detail/components/projectInfo.jsx
@@ -1,24 +1,40 @@
 "use client";
+import { Dialog } from "@/components/ui/dialog";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ProjectInfo({ project }) {
-  const [rewardsBtn, setRewardsBtn] = useState(false);
   const router = useRouter();
+  const token = sessionStorage.getItem("accessToken");
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY >= 1090);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll(); // 처음 로드 시에도 한 번 실행
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const handlePledge = (rewardId) => {
-    router.push(`/project/detail/${project.projectNo}/pledge?rewardId=${rewardId}`);
+    if (token) {
+      router.push(`/project/detail/${project.projectNo}/pledge?rewardId=${rewardId}`);
+    } else {
+      router.push("/seokgeun");
+    }
   };
 
   return (
-    <section className="w-[352px] pt-[25px]">
+    <section className={`w-[352px] pt-[25px]  `}>
       <div className="w-full min-h-[500px]">
         <div className="w-full mb-6">
           <Image src={"/jungho/projectDetail-img-1.png"} alt="pc 이미지" width={352} height={97} />
         </div>
-
+        {/* 창작자 정보 */}
         <div
           className="border border-[#e4e4e4] rounded-[12px] p-5"
           style={{ boxShadow: "rgba(0, 0, 0, 0.03) 0px 1px 0px, rgba(0, 0, 0, 0.1) 0px 1px 6px" }}
@@ -36,12 +52,16 @@ export default function ProjectInfo({ project }) {
             <p className="text-[#6d6d6d] font-normal text-[13px] leading-[22px]">{project.creatorInfo}</p>
           </div>
         </div>
-
-        <div className="pt-6">
+        {/* 선물 선택 */}
+        <div
+          className={`pt-6 transition-all duration-300 ease-in-out ${
+            isScrolled ? "fixed top-[50px] right-[13%] w-[352px] z-10" : ""
+          }`}
+        >
           <p className="text-sm text-[#3d3d3d] font-semibold mb-[0.5rem]">선물 선택</p>
           <div className="flex flex-col gap-5">
             <button className="text-left" onClick={(e) => alert("준비중입니다")}>
-              <div className="border p-5 rounded-md">
+              <div className="border p-5 rounded-md shadow-[0px_1px_0px_rgba(0,0,0,0.1),_0px_2px_4px_rgba(0,0,0,0.04)]">
                 <p className="text-2xl leading-[36px] mb-[6px] tracking-[-0.025em]">1000원 +</p>
                 <p className="text-[13px] leading-[20px]">선물 없이 후원하기</p>
               </div>
@@ -50,7 +70,7 @@ export default function ProjectInfo({ project }) {
               project.rewards.map((item) => (
                 <button
                   key={item.id}
-                  className="border p-5 rounded-md text-left"
+                  className="border p-5 rounded-md text-left shadow-[0px_1px_0px_rgba(0,0,0,0.1),_0px_2px_4px_rgba(0,0,0,0.04)]"
                   onClick={(e) => handlePledge(item.id)}
                 >
                   <p className="font-semibold">{item.title}</p>
